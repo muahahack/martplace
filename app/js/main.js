f6fc4b12200816ef9ffb91598f25181ba9d392ed
muahahack <muahahack@ukr.net>
2020-07-18 22:29:05 +0300

    modify previos site, beta main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,66 @@
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+var mySwiper = new Swiper('.feed-container', {
+  wrapperClass: 'feed-wrapper',
+  slideClass: 'feed-slide',
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.feed-button-next',
+    prevEl: '.feed-button-prev',
+  }
+});
+
+var mySwiper = new Swiper('.feedback__slider', {
+  wrapperClass: 'feedback__items',
+  slideClass: 'feedback__item',
+  slidesPerView: 2,
+  loop: true,
+  spaceBetween: 28,
+  navigation: {
+    nextEl: '.feedback-button-next',
+    prevEl: '.feedback-button-prev',
+  }
+});
+
 $(function(){
 
   $(".weekly-products__specs-rating").rateYo({
-    starWidth: "15px",
+    starWidth: "14px",
     rating: 4.5,
-    spacing: "4px"
+    spacing: "4px",
+    ratedFill: "#FFC000",
+    normalFill: "#FFCD37"
 
   });
 
   $(".newest-products__specs-rating").rateYo({
     starWidth: "13px",
     rating: 4.5,
-    spacing: "4px"
+    spacing: "4px",
+    ratedFill: "#FFC000",
+    normalFill: "#FFCD37"
+
+  });
+
+  $(".feed-products__specs-rating").rateYo({
+    starWidth: "13px",
+    rating: 4.5,
+    spacing: "4px",
+    ratedFill: "#FFC000",
+    normalFill: "#FFCD37"
 
   });
 
   var mixer = mixitup('.newest-products__items-wrapper');
 
 });
-
-
-// starSvg: "<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>"
--- app/js/slider.js
+++
@@ -1,8 +0,0 @@
-var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
